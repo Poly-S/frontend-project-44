65162653b04c62fd05129ff952001646ac4a5167
root <root@Poly-S>
2024-01-28 19:03:04 +0300

    add 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+
 import runGame from '../src/cli.js';
 
 runGame();
-
+// eslint-disable-next-line no-console
 console.log('Hello, Brain Games!');
-
+// eslint-disable-next-line no-console
 console.log('Welcome to the Brain Games!');
