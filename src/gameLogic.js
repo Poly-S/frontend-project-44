4322a8f4f24865b38a47a9e6ad2589c456e61707
root <root@Poly-S>
2024-02-16 13:24:09 +0300

    correction

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -3,25 +3,25 @@ import readlineSync from 'readline-sync';
 export const roundsNumber = 3;
 
 const greetAndGetLogic = (rounds, description) => {
-	console.log('Welcome to the Brain Games!');
-	const name = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
-	console.log(description);
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(description);
 
-	for (let i = 0; i < roundsNumber; i += 1) {
-		const [question, answer] = rounds[i];
-		console.log(`Question: ${question}`);
-		const userAnswer = readlineSync.question('Your answer: ');
+  for (let i = 0; i < roundsNumber; i += 1) {
+  const [question, answer] = rounds[i];
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
 
-		if (userAnswer === answer) {
-			console.log('Correct!');
-		} else {
-			console.log(`'${userAnswer}' is the wrong answer. The correct answer was '${answer}'.`);
-			console.log(`Let's try again, ${name}!`);
-			return;
-		}
-	}
-	console.log(`Congratulations, ${name}!`);
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is the wrong answer. The correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 };
 
 export default greetAndGetLogic;
