74a4b6b678c6829d27632a097db62b3e9840b756
root <root@Poly-S>
2024-03-23 16:48:24 +0300

    correction

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-const getRandomNumber = (min, max = 100) => {
-	return Math.floor(min + Math.random() * (max + 1 - min));
+export const getRandomNumber = (min, max = 100) => {
+  return Math.floor(min + Math.random() * (max + 1 - min));
 };
-
-export { getRandomNumber };
